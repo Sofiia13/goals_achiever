61e5fdb2dfd507694597fa61b2aee6794c73574a
Sofiia13 <sophiyastanish@gmail.com>
2025-12-05 19:58:38 +0200

    add quote component

---
+++ frontend/src/shared/components/ui/Quote/Quote.tsx
@@ -0,0 +1,6 @@
+import type React from "react";
+import styles from "./Quote.module.scss";
+
+export const Quote: React.FC = () => {
+  return <div className={styles.quote}>Some text of quote</div>;
+};
