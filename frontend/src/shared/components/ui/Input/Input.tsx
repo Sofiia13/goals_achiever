8ee1e15f3dcab3ecf8f37630bb294d41e788d111
Sofiia13 <sophiyastanish@gmail.com>
2025-12-15 20:41:13 +0200

    fix input component bug

--- frontend/src/shared/components/ui/Input/Input.tsx
+++ frontend/src/shared/components/ui/Input/Input.tsx
@@ -6,23 +6,36 @@ type InputProps = {
   onChange: (value: string) => void;
   placeholder?: string;
   type?: string;
+  label?: string;
   className?: string;
+  id?: string;
 };
 
 export const Input: React.FC<InputProps> = ({
-  onChange,
+  onChange, 
   placeholder = "",
   type = "text",
+  label = "",
   className = "",
+  id,
 }) => {
   const inputClass = `${styles.input} ${className}`;
 
+  const inputId = id || label?.toLowerCase().replace(/\s+/g, "-");
+
   return (
-    <input
-      className={inputClass}
-      type={type}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder={placeholder}
-    />
+    <>
+      {label && (
+        <label htmlFor={inputId} className={styles.label}>
+          {label}
+        </label>
+      )}
+      <input
+        className={inputClass}
+        type={type}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+      />
+    </>
   );
 };
