e86b19c58d50ede0396d136e835220470d2f78de
Sofiia13 <sophiyastanish@gmail.com>
2026-03-16 13:38:36 +0200

    try to fix deploy issues

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
-import App from "./app/App.tsx";
+import App from "./app/App";
 import "./styles/_globals.scss";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./shared/context/AuthContext.tsx";
+import { AuthProvider } from "./shared/context/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
